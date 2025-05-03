3c42a6e7450bc79be9e7575ad4d8eca34dfe6647
Therozental <160230574+Therozental@users.noreply.github.com>
2025-05-03 19:18:09 +0300

    update timeline version

--- Assets/TimeLine/Ruler/TimeRuler.cs
+++ Assets/TimeLine/Ruler/TimeRuler.cs
@@ -27,22 +27,23 @@ public class TimeRuler : MonoBehaviour
 
     private void PositionTicks()
     {
-        float timeCovered = gameObject.GetComponent<SpriteRenderer>().bounds.size.x / TimeLine.Instance.trackLengthFor1Second;
+       float timeCovered = TimeLine.Instance.timeLineSeconds;
+       // float timeCovered = gameObject.GetComponent<SpriteRenderer>().bounds.size.x / TimeLine.Instance.trackLengthFor1Second;
         float rulerBottom = gameObject.GetComponent<SpriteRenderer>().bounds.min.y;
         HashSet<float> bigTickPositions = new HashSet<float>();
         // position large ticks
-        for (float i = 0; i <= timeCovered; i += bigTickFrequency)
+        for (float i = bigTickFrequency; i < timeCovered; i += bigTickFrequency)
         {
             float tickXPos = transform.position.x + i * TimeLine.Instance.trackLengthFor1Second;
             bigTickPositions.Add(tickXPos);
             Vector3 bigTickPos = new Vector3(tickXPos, rulerBottom, transform.position.z);
             GameObject bigTick = Instantiate(bigTickPrefab, bigTickPos, Quaternion.identity, ticksSceneParent);
                 //          if(i == 0) 
-//                bigTick.GetComponentInChildren<TextMeshPro>().enabled = false; // no text for first tick
-  //          bigTick.GetComponentInChildren<TextMeshPro>().text = i.ToString("0.00"); // format to 2 decimal places
+         //                bigTick.GetComponentInChildren<TextMeshPro>().enabled = false; // no text for first tick
+            //          bigTick.GetComponentInChildren<TextMeshPro>().text = i.ToString("0.00"); // format to 2 decimal places
         }
         // position small ticks
-        for (float i = 0; i <= timeCovered; i += smallTickFrequency)
+        for (float i = smallTickFrequency; i < timeCovered; i += smallTickFrequency)
         {
             float tickXPos = transform.position.x + i * TimeLine.Instance.trackLengthFor1Second;
             if (bigTickPositions.Contains(tickXPos))
