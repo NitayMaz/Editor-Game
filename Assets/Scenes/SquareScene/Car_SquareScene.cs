25ffeaf0a8cef5593ba7dc4495077e3d4322a244
Hili64 <hilimiz23@gmail.com>
2025-06-02 10:14:59 +0300

    Square scene added

---
+++ Assets/Scenes/SquareScene/Car_SquareScene.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+
+public class Car_SquareScene : TrackControlled
+{
+    [SerializeField] ParticleSystem waiterCollisionEffect;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if(!TimeLine.Instance.isPlaying)
+            return;
+        waiterCollisionEffect.transform.position = transform.position;
+        waiterCollisionEffect.Play(); //particle!
+        Debug.Log(other.name.ToString());
+        StartInteraction();
+        StageManager.Instance?.StageFailed();
+    }
+
+
+    public override void StartInteraction()
+    {
+        Vector3 pos = transform.position;
+        base.StartInteraction();
+        animator.enabled = false;
+        transform.position = pos;
+    }
+
+    public override void StopInteraction()
+    {
+        base.StopInteraction();
+    }
+}
