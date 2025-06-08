dfb07e11178f58248dba3cfad54a96d95a6fe4bf
Nitay Mazig <nitay.maz@gmail.com>
2025-06-09 00:03:07 +0300

    football scene fix

--- Assets/Scenes/Football Scene/scripts/SoccerStageManager.cs
+++ Assets/Scenes/Football Scene/scripts/SoccerStageManager.cs
@@ -3,10 +3,16 @@ using UnityEngine;
 public class SoccerStageManager : StageManager
 {
     [SerializeField] private Rigidbody2D ballRigidbody;
-    
+
+    public override void StageReset()
+    {
+        base.StageReset();
+        // ballRigidbody.linearDamping = 0f;
+    }
+
     public override void StageSuccess()
     {
-        ballRigidbody.linearDamping = 2f;
+        // ballRigidbody.linearDamping = 2f;
         base.StageSuccess();
         Invoke(nameof(ShowStageSuccessUI), 2f);
     }
