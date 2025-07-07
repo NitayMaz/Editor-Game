22360892a17c140466fc4935ca355d1c44320b95
Nitay Mazig <nitay.maz@gmail.com>
2025-07-08 00:57:04 +0300

    fixing cursor ui jump

--- Assets/UI/CursorUI.cs
+++ Assets/UI/CursorUI.cs
@@ -3,11 +3,20 @@ using UnityEngine;
 public class CursorUI : MonoBehaviour
 {
     [SerializeField] private RectTransform cursorImage;
-    [SerializeField] private Vector2 offset = new Vector2(0, 0);
+    [SerializeField] private Canvas canvas;
     
     void Update()
     {
         Vector2 mousePos = Input.mousePosition;
-        cursorImage.position = mousePos + offset;
+        Vector2 localPoint;
+
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            canvas.transform as RectTransform,
+            mousePos,
+            canvas.renderMode == RenderMode.ScreenSpaceOverlay ? null : canvas.worldCamera,
+            out localPoint
+        );
+
+        cursorImage.anchoredPosition = localPoint;
     }
 }
