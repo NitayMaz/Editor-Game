9e7055eeae3e768b8168c758b5bd110d0aceacd1
Nitay Mazig <nitay.maz@gmail.com>
2025-07-07 19:39:19 +0300

    fade in fade out

--- Assets/UI/NextStageButton.cs
+++ Assets/UI/NextStageButton.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class NextStageButton : MonoBehaviour
 {
     [SerializeField] private Animator animator;
+    [SerializeField] private float fadeOutDuration = 0.5f;
 
     private void OnMouseOver()
     {
@@ -17,6 +19,13 @@ public class NextStageButton : MonoBehaviour
     private void OnMouseDown()
     {
         SoundManager.Instance.PlayAudio(AudioClips.MouseClick);
+        animator.SetTrigger("FadeOut");
+        StartCoroutine(MoveToNextStage());
+    }
+    
+    private IEnumerator MoveToNextStage()
+    {
+        yield return new WaitForSeconds(fadeOutDuration);
         StageManager.Instance.MoveToNextStage();
     }
 }
